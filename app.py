a54958d57983116f8acec3def993640a415ce79e
RodrigoFP51 <rodrigopizzinato@hotmail.com>
2024-03-12 21:06:58 -0300

    model pipeline included datetime features

--- app.py
+++ app.py
@@ -1,30 +1,102 @@
-from flask import Flask, jsonify, request
-import pandas as pd 
+import pandas as pd
+from numpy import exp
+import streamlit as st
 import joblib
-
-def prep_data(data):
-    data = data.drop(['id','host_id','host_name'],axis=1)
-    data['price'] = data['price'].astype("float64")
+import os
     
-    for col in data.select_dtypes(include='object').columns[1:]:
-        data[col] = data[col].astype("category")
+model = joblib.load(open('models/best_lgbm.pkl','rb'))
+
+data = pd.read_csv('https://raw.githubusercontent.com/RodrigoFP51/nyc_ayrbnb_prices/main/Data/airbnb_imputed.csv')
+neigh_groups = data['neighbourhood_group'].unique()
+room_type = data['room_type'].unique()
+neighbourhood = data['neighbourhood'].unique()
+
+# model.feature_names_in_
+
+st.title('Predict Airbnb Rent Cost')
+col1, col2 = st.columns([1,1])
+
+with col1:
+    latitude = st.slider(
+        "Latitude",
+        value=50,
+        min_value=30,
+        max_value=150,
+        step=1
+    )
     
-    data['last_review'] = pd.to_datetime(data['last_review'])
+    longitude = st.slider(
+        "Longitude",
+        value=50,
+        min_value=30,
+        max_value=150,
+        step=1
+    )
     
-    data['year'] = data.last_review.dt.year
-    data['month'] = data.last_review.dt.month
-    del data['last_review']
+    min_nights = st.number_input(
+        "Minimum Nights"
+    )
     
-    return data
+    num_reviews = st.number_input(
+        "Number of Reviews"
+    )
+    
+    reviews_per_month = st.number_input(
+        "Reviews per Month"
+    )
+    
+    calculated_host_listings_count = st.number_input(
+        "Listing Count"
+    )
+    
+    availability_365 = st.number_input(
+        "Availability"
+    )
+    
+with col2:
+    description = st.text_input("Description")
+    
+    neighbourhood_group = st.selectbox(
+        "Neighbourhood Group",
+        options=neigh_groups
+    )
 
-app = Flask(__name__)
+    neighbourhood = st.selectbox(
+        "Neighbourhood",
+        options=neighbourhood
+    )
+    
+    room_type = st.selectbox(
+        "Room Type",
+        options=room_type
+    )
+    
+    last_review = st.date_input(
+        "Last Review"
+    )
+    
+    
 
-@app.route('/', methoods=['POST'])
 
-model = joblib.load('models/best_lgbm.pkl')
-data = pd.read_csv('Data/airbnb_imputed.csv')
-data = prep_data(data)
+client_data = pd.DataFrame({
+      "latitude": [latitude],
+      "longitude": [longitude],
+      "minimum_nights": [min_nights],
+      "number_of_reviews": [num_reviews],
+      "reviews_per_month": [reviews_per_month],
+      "calculated_host_listings_count": [calculated_host_listings_count],
+      "availability_365": [availability_365], 
+      "name": [description],
+      "neighbourhood_group": [neighbourhood_group],
+      "neighbourhood": [neighbourhood],
+      "room_type": [room_type],
+      "last_review": [last_review]
+})
 
-model.predict(data.drop(['price'],axis=1).sample(20))
+st.dataframe(client_data)
+
+pred_value = model.predict(client_data)
+pred_value = exp(pred_value)
+st.write(f"Valor do aluguel previsto: US{round(float(pred_value), 2)}")
 
 
